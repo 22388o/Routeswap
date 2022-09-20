efd89ff102432eba6af6409bb926f60c5cb27a77
leffw <93302536+leffw@users.noreply.github.com>
2022-09-20 03:15:11 -0300

    Update lightning.py

--- services/lightning.py
+++ services/lightning.py
@@ -1,6 +1,6 @@
 from services import lnd
 from configs import LN_BACKEND
 
-def create_invoice(amount: float, memo="", expiry=86400, metadata={}):
+def create_invoice(amount: float, memo="", expiry=86400, metadata={}, typeof="loop-out") -> dict:
     if (LN_BACKEND == "lnd"):
-        return lnd.create_invoice(amount, memo=memo, expiry=expiry, metadata=metadata)
+        return lnd.create_invoice(amount, memo=memo, expiry=expiry, metadata=metadata, typeof=typeof)
