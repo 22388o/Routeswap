c8b24f8c4f512166654a9cb74eeb649b0b6164bf
leffw <93302536+leffw@users.noreply.github.com>
2022-09-22 20:30:03 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,27 +1,67 @@
-from models.schemas import SwapSchema
-from services import swap
-from configs import API_HOST, API_PORT
+from fastapi.middleware.cors import CORSMiddleware
+from services.bitcoin import get_estimate_fee, get_new_address, bitcoin
+from helpers.helpers import sats_to_btc
+from models.schemas import LoopinSchema, LoopoutSchema
+from services import loop
+from configs import API_HOST, API_PORT, LOOP_MIN_BTC, SERVICE_FEE_RATE, SERVICE_MIN_FEE_RATE
 from fastapi import FastAPI, HTTPException
 
 import uvicorn
 
 api = FastAPI()
+api.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-@api.post("/api/v1/swap/create")
-def create_new_swap(data: SwapSchema):
-    swap_details_payment = swap.create_swap(data.base, data.quote, data.address, data.amount, data.feerate)
-    if (swap_details_payment.get("message") != None):
-        raise HTTPException(500, detail=swap_details_payment["message"])
-    else:
-        return swap_details_payment
+@api.post("/api/v1/loop/out")
+def loop_out(data: LoopoutSchema):
+    address = data.address
+    feerate = data.feerate
+    amount = data.amount
 
-@api.get("/api/v1/lookup/{txid}")
-def get_lookup(txid):
-    lookup = swap.get_lookup(txid)
-    if (lookup.get("message") != None):
-        raise HTTPException(500, detail=lookup["message"])
+    loop_out = loop.create_loop_out(address, amount, feerate=feerate)
+    if (loop_out.get("message") != None):
+        raise HTTPException(500, detail=loop_out["message"])
     else:
-        return lookup
+        return loop_out
+
+@api.post("/api/v1/loop/in")
+def loop_in(data: LoopinSchema):
+    loop_in = loop.create_loop_in(data.invoice)
+    if (loop_in.get("message") != None):
+        raise HTTPException(500, detail=loop_in["message"])
+    else:
+        return loop_in
+
+@api.get("/api/v1/tx/{txid}")
+def get_transaction(txid: str):
+    tx = loop.get_transaction(txid)
+    if (tx.get("message") != None):
+        raise HTTPException(500, detail=tx["message"])
+    else:
+        return tx
+
+@api.get("/api/v1/info")
+def info():
+    return {"LOOP_MIN_BTC": LOOP_MIN_BTC, "SERVICE_FEE_RATE": SERVICE_FEE_RATE, "SERVICE_MIN_FEE_RATE": SERVICE_MIN_FEE_RATE,}
+
+@api.get("/api/v1/estimate/fee")
+def estimate_fee(address: str = None, amount: float = 0, feerate: float = 1):
+    if (address == None) or (amount == 0):
+        raise HTTPException(500)
+
+    if (bitcoin.validate_address(address)["isvalid"] == False):
+        address = get_new_address()["address"]
+
+    amount = int(amount * pow(10, 8))
+    estimate_fee = get_estimate_fee(address, amount)
+    if not (estimate_fee.get("fee_sat")):
+        raise HTTPException(500)
     
+    fee_sat = int(estimate_fee["fee_sat"])
+    feerate_sat_per_byte = int(estimate_fee["feerate_sat_per_byte"])
+    
+    fee_sat = int((fee_sat / feerate_sat_per_byte) * feerate)
+    fee_btc = sats_to_btc(fee_sat)
+    return {"fee_sats": fee_sat, "fee_btc": fee_btc}
+
 def start():
     uvicorn.run(api, host=API_HOST, port=API_PORT)
